7ead8adef794219a6099c2bb260cb1c24b194a26
DirtbagDrew <andrewmyerhb@gmail.com>
2019-04-01 00:11:08 -0700

    added social media icons and semicolon seperated list for email

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
      if($_POST['email']!=""){
-        $data=$_POST['email'];
+        $data = $_POST['email'];
+        $data .= "; ";
         $ret = file_put_contents('file.txt', $data, FILE_APPEND | LOCK_EX);
         if($ret === false) {
             die('There was an error writing this file');
         }
         else {
-            echo "$ret bytes written to file";
         }
     
         }}
@@ -35,6 +35,7 @@ function checkEmpty() {
     <title>Daily 49er</title>
     <link rel="stylesheet" href="bower_components/FlipClock/compiled/flipclock.css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="./style.css" />
     <script src="jquery-3.3.1.min.js"></script>
 </head>
@@ -50,27 +51,37 @@ function checkEmpty() {
                             style="font-size:200%;zoom:1;margin-left: 0; display: inline-block; text-align:center">
                         </flip-clock>
                     </div>
+                    <div style="background:white; border-radius: 10px;">
+                        <div style="margin:15px;">
+                            <form name="myForm"
+                                style="margin: 0;zoom:1.5;display: inline-block,"
+                                method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
+                                onsubmit="return checkEmpty()">
 
-                    <form name="myForm"
-                        style="background:white; padding: 10px;border-radius: 10px;margin: 0;zoom:1.5;display: inline-block,"
-                        method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"
-                        onsubmit="return checkEmpty()">
-
-                        <div class=" form-group form-group-lg" style="background:none">
-                            <label for="exampleInputEmail1">Be the first to know when the new Daily 49er website
-                                launches!</label>
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
-                                name="email" placeholder="Enter email" />
+                                <div class=" form-group form-group-lg" style="background:none">
+                                    <label for="exampleInputEmail1">Be the first to know when the new Daily 49er website
+                                        launches!</label>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                                        name="email" placeholder="Enter email" />
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <?php 
+                                if($_SERVER["REQUEST_METHOD"] == "POST"){
+                                    if($_POST['email']!=""){
+                                        echo '<div class="alert alert-success" role="alert" id="success" style="margin:7px auto;">Submission Successful!</div>';
+                                    }
+                                }
+                            ?>
+                            </form>
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                        <?php 
-                        if($_SERVER["REQUEST_METHOD"] == "POST"){
-                            if($_POST['email']!=""){
-                                echo '<div class="alert alert-success" role="alert" id="success" style="margin:7px auto;">Submission Successful!</div>';
-                            }
-                        }
-                    ?>
-                    </form>
+                        <div style="background-color:#f7c309; width:100%; border-radius: 0 0 10px 10px; margin-top:10px; padding:2px;">
+                            <h1 style="margin-top:0;font-family:Helvetica Neue,Helvetica,Arial,sans-serif; font-size:20px; font-weight:700">Don't forget to follow us on social media!</h1>
+                            <a href="https://twitter.com/daily49er" target="_blank" style="margin:4px; color:black;"><i class="fab fa-twitter fa-lg"></i></a>
+                            <a href="https://www.instagram.com/daily49er/" target="_blank" style="margin:4px; color:black;"><i class="fab fa-instagram fa-lg"></i></a>
+                            <a href="https://www.facebook.com/daily49er" target="_blank" style="margin:4px; color:black;"><i class="fab fa-facebook-f fa-lg"></i></a>
+                            <a href="https://www.youtube.com/user/videod49er/" target="_blank" style="margin:4px; color:black;"><i class="fab fa-youtube fa-lg"></i></a>
+                        </div>
+                    </div>
 
                 </div>
             </div>
